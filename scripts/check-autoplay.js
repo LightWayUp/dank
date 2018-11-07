f27083ffa84b1ef78db93ee9ac6bde4475dbd8df
LightWayUp <lightwayuplightwayup@gmail.com>
2018-11-07 21:32:51 +0800

    Rely on Promise to check for autoplay availibility
    - Rely on Promise to check for autoplay availibility, old hacky method is now used as fallback
    - Fixed prepareAlert() called twice
    - Added dir attribute to html element
    - Set window.onerror to log a message about opening new issue in repository to console

--- scripts/check-autoplay.js
+++ scripts/check-autoplay.js
@@ -27,6 +27,7 @@
 
 /**
  * A boolean value to determine if alert dialog should be shown.
+ * Only used when the fallback method {@link #prepareAlert} is called.
  * If audio starts playing automatically, {@link #cancelAlert} is called,
  * setting this value to "false".
  * 
@@ -34,17 +35,6 @@
  * @type {boolean}
  */
 var shouldAlert = true;
-
-/**
- * A boolean value to determine if {@link #prepareAlert} has been called.
- * The mentioned method is triggered by the "oncanplaythrough" event from audio element.
- * As the event seems to fire multiple times, this value allows for checking if the
- * method was previously called. Upon method call, this is set to "true".
- * 
- * @default false
- * @type {boolean}
- */
-var isPrepareAlertCalled = false;
 
 /**
  * An Element representing the audio element in HTML DOM.
@@ -62,79 +52,119 @@ var audioElement;
 const MESSAGE = "Your internet browser doesn't seem to support or allow audio autoplay.\nOnly half of the dankness for you, sorry \uD83D\uDE26!";
 
 /**
+ * Log a message to the console when an Error is thrown.
+ * @function
+ * @kind function
+ * @type {function}
+ * @listens onerror
+ */
+window.onerror = function(message, source, lineNumber, columnNumber, error) {
+    console.log(`An error occured, please report this at https://github.com/VanishedApps/dank/issues/new .\n\nFull details, copy and paste into issue description:\n\n${error.stack}`);
+}
+
+/**
  * Cancel the alert by setting {@link #shouldAlert} to "false",
  * after being triggered by the "ontimeupdate" event.
- * This also calls {@link #removeAttrForAudioElement} to remove attributes
- * for listening to events from the audio element.
  * @listens ontimeupdate
  */
 function cancelAlert() {
     shouldAlert = false;
-    removeAttrForAudioElement();
+}
+
+/**
+ * Attempt to play audio when the page is loaded.
+ * If autoplay is disabled or unsupported, an alert dialog is shown.
+ * 
+ * If calling function "play()" does not return a Promise,
+ * fallback method {@link #prepareAlert} is used.
+ * @listens onload
+ */
+function playAudio() {
+    // Load audio element
+    initializeAudioElement();
+    const promise = audioElement.play();
+    // Check if browser returns a Promise
+    if (promise !== undefined) {
+        // Check if audio can start playing
+        promise.then(
+            // Audio can start playing, do nothing
+            function() {},
+            // Audio can't start playing, show the alert dialog
+            function(reason) {
+                showAlert(reason.toString());
+        });
+    } else {
+        // Browser does not return a Promise, use hacky fallback method
+        // to determine if audio is playing
+        prepareAlert();
+    }
 }
 
 /**
  * Get the "audio" element from DOM and initialize {@link #audioElement}.
+ * @throws {Error} Throws an Error if the correct audio element can't be found.
  */
 function initializeAudioElement() {
     // Get all elements with "audio" tag from DOM as an array
     const audioElements = document.getElementsByTagName("audio");
-    // Check if there are any such element
-    if (audioElements.length === 0) {
-        // None is found as array doesn't contain anything
-        console.log("Can not find element with tag name \"audio\"!");
-    } else {
-        // Initialize the audioElement object
-        // Index is 0 as there should be only one audio element
-        audioElement = audioElements[0];
+    // Check if there is exactly 1 audio element
+    if (audioElements.length !== 1) {
+        // None or more than one are found
+        throw new Error("Can not find the correct element with tag name \"audio\"!");
     }
+    // Initialize the audioElement object
+    // Index is 0 as there is only one audio element
+    audioElement = audioElements[0];
 }
 
 /**
- * Start the timeout timer. This is triggered by the "oncanplaythrough" event.
+ * Start the timeout timer. This is a fallback method for browsers that
+ * does not return a Promise when "play()" is called.
  * If no audio is playing after the 1 second timeout, the alert dialog is shown,
  * notifying the site visitor that autoplay for audio is unavailable.
  * 
- * Once the entire audio track can play through without buffering,
- * the "oncanplaythrough" event is fired. This function is then called.
- * As the event seems to fire multiple times, a check is first performed
- * to see if this was previously called, by using {@link #isPrepareAlertCalled}.
- * If not, it initializes {@link #audioElement} with {@link #initializeAudioElement}.
- * Then, it starts a timeout of 1000 milliseconds. Upon timeout, it checks
+ * It starts a timeout of 1000 milliseconds. Upon timeout, it checks
  * {@link #shouldAlert} to determine if alert dialog should be shown.
- * This also calls {@link #removeAttrForAudioElement} to remove attributes
- * for listening to events from the audio element.
+ * This also calls {@link #removeAttrForAudioElement} to remove the attribute
+ * for listening to "ontimeupdate" event from the audio element.
  * @listens oncanplaythrough
  */
 function prepareAlert() {
-    // Check if this function is called before
-    if (!isPrepareAlertCalled) {
-        // It had never been called, now set isPrepareAlertCalled to true
-        isPrepareAlertCalled = true;
-        // Initialize audioElement
-        initializeAudioElement();
-        // Set the 1000 milliseconds timeout as audio track is fully loaded
-        setTimeout(function() {
-            // Check if audio is already playing
-            if (shouldAlert) {
-                // No audio is playing, show the alert dialog
-                alert(MESSAGE);
-                // Stop listening to events
-                removeAttrForAudioElement();
-            }
-        }, 1000);
-    }
+    setTimeout(function() {
+        // Check if audio is already playing
+        if (shouldAlert) {
+            // No audio is playing, show the alert dialog
+            showAlert();
+        }
+        // Stop listening to event
+        removeAttrForAudioElement();
+    }, 1000);
 }
 
 /**
- * Remove event listeners for both "oncanplaythrough" and "ontimeupdate" events
+ * Show the alert dialog. Optionally, a reason can be included.
+ * @param {string} reason The reason audio can not start playing automatically.
+ * @throws {TypeError} Throws a TypeError if type of reason is not string.
+ */
+function showAlert(reason) {
+    if (typeof reason !== "string") {
+        throw new TypeError("Reason must be a string!");
+    }
+    var alertMessage = MESSAGE;
+    if (reason !== undefined && reason !== null) {
+        alertMessage += `\n\nReason:\n${reason}`;
+    }
+    alert(alertMessage);
+}
+
+/**
+ * Remove event listeners for "ontimeupdate" event
  * from the audio element to free up system resource.
  * If {@link #audioElement} is never initialized successfully,
  * this function does nothing.
  */
 function removeAttrForAudioElement() {
     if (typeof audioElement !== "undefined") {
-        audioElement.removeAttribute("oncanplaythrough");
         audioElement.removeAttribute("ontimeupdate");
     }
 }
