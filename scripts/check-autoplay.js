7462360aff3ae26653fca802d3c0791b220701f3
LightWayUp <lightwayuplightwayup@gmail.com>
2019-04-04 18:38:23 +0800

    Fixed showAlert() reason not being optional
    - Fixed argument "reason" of function showAlert() being mandatory instead of optional
    - Accept Error as argument for function showAlert()
    - Added unboxIfBoxed() function to unbox wrapper type objects
    - Added getRepository() function to get repository name and owner name dynamically
    - Miscellaneous improvements

--- scripts/check-autoplay.js
+++ scripts/check-autoplay.js
@@ -43,11 +43,19 @@ let audioElement;
 const MESSAGE = "Your internet browser doesn't seem to support or allow audio autoplay.\nOnly half of the dankness for you, sorry \uD83D\uDE26!";
 
 /**
+ * The repository this website is for, in "owner/repository" format.
+ * @constant {string}
+ * @default
+ * @readonly
+ */
+const REPOSITORY_REPRESENTATION = getRepository().representation;
+
+/**
  * Log a message to the console when an Error is thrown.
  * @function
  * @listens onerror
  */
-window.onerror = (message, source, lineNumber, columnNumber, error) => console.log(`An error occured, please report this at https://github.com/VanishedApps/dank/issues/new .\n\nFull details, copy and paste into issue description:\n\n${error}`);
+window.onerror = (message, source, lineNumber, columnNumber, error) => console.log(`An error occured, please report this at https://github.com/${REPOSITORY_REPRESENTATION}/issues/new .\n\nFull details, copy and paste into issue description:\n\n${error}`);
 
 /**
  * Attempt to play audio when the page is loaded.
@@ -59,7 +67,7 @@ window.onload = () => {
     // Load audio element
     initializeAudioElement();
     // Play audio and handle rejection
-    audioElement.play().catch(reason => showAlert(reason.toString()));
+    audioElement.play().catch(showAlert);
 }
 
 /**
@@ -78,16 +86,46 @@ function initializeAudioElement() {
 
 /**
  * Show the alert dialog. Optionally, a reason can be included.
- * @param {string|String} reason The reason audio can not start playing automatically.
+ * @param {string|String|Error} [reason] The optional reason audio
+ * can not start playing automatically.
  * @throws {TypeError} Throws a TypeError if type of reason is not string.
  */
 function showAlert(reason) {
-    if (!(typeof reason === "string" || reason instanceof String)) {
+    reason = reason instanceof Error ? reason.toString() : unboxIfBoxed(reason);
+    if (!(typeof reason === "string" || reason == null)) {
         throw new TypeError("Reason must be a string!");
     }
-    let alertMessage = MESSAGE;
-    if (reason != null) {
-        alertMessage += `\n\nReason:\n${reason.valueOf()}`;
+    alert(reason == null ? MESSAGE : `${MESSAGE}\n\nReason:\n${reason}`);
+}
+
+/**
+ * Get the name of repository, name of owner and repository's representation.
+ * @returns {object} An object containing information about name of owner,
+ * name of repository, and its representation.
+ */
+function getRepository() {
+    const location = window.location;
+    const owner = location.hostname.split(/\./gi)[0];
+    const repository = location.pathname.split(/\//gi)[1];
+    return {
+        owner: owner,
+        repository: repository,
+        representation: `${owner}/${repository}`
+    };
+}
+
+/**
+ * Unbox Number, Boolean and String objects.
+ * @param {} object The object to be unboxed. If it isn't
+ * an instance of Number, Boolean, or String, the original
+ * object or value is returned.
+ * @returns {} Value of unboxed Numbers, Booleans, or Strings.
+ * The original object or value is returned if it isn't
+ * an instance of Number, Boolean, or String.
+ */
+function unboxIfBoxed(object) {
+    if (object instanceof Number || object instanceof Boolean || object instanceof String) {
+        return object.valueOf();
     }
-    alert(alertMessage);
+    return object;
 }
